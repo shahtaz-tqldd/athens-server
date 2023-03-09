c5c4832712702ac1b4a661a87e35ba4f7f94e1f4
Shahatz Rahman <shahtazrahman17@gmail.com>
2023-03-10 02:20:24 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express")
 const cors = require("cors")
 const app = express()
@@ -8,10 +9,29 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req,res)=>{
+// MONGODB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1uor19o.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const postCollection = client.db('athens').collection('posts')
+
+        //posts ===========================
+        app.get('/posts', async (req, res) => {
+            const result = await postCollection.find({}).toArray()
+            res.send(result)
+        })
+
+    } finally { }
+}
+run().catch(err => console.error(err))
+
+
+app.get('/', (req, res) => {
     res.send("Athens server is running...")
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Athens server is running on ${port}`)
 })
