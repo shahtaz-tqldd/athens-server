3859e26b76e0ce99af50903d3a40e6b8385a2dc5
Shahatz Rahman <shahtazrahman17@gmail.com>
2023-03-10 01:54:26 +0600

    athens server created

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+// MIDDLEWARE
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res)=>{
+    res.send("Athens server is running...")
+})
+
+app.listen(port, ()=>{
+    console.log(`Athens server is running on ${port}`)
+})
